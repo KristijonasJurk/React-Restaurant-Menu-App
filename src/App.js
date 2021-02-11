04ba3bedab98532ae881634dc8487f2c5f990709
KristijonasJurk <70882614+KristijonasJurk@users.noreply.github.com>
2021-02-11 21:25:29 +0200

    finished categories

--- src/App.js
+++ src/App.js
@@ -7,13 +7,14 @@ const allCategories = ['all', ...new Set(items.map((item) => item.category))];
 
 function App() {
   const [menu, setMenu] = useState(items)
-  const [categories, setCategories] = useState([])
+  const [categories, setCategories] = useState(allCategories)
 
   const filterItems = (category) => {
-    if (category == 'all') {
-      setMenu(menu)
+    if (category === 'all') {
+      setMenu(items);
+      return;
     }
-    const newItems = menu.map((item) => item.category !== category)
+    const newItems = items.filter((item) => item.category === category)
     setMenu(newItems)
   }
 
@@ -26,8 +27,8 @@ function App() {
         </h2>
           <div className="underline"></div>
         </div>
-        <Categories filterItems={filterItems} allCategories={allCategories} />
-        <Menu items={items} />
+        <Categories filterItems={filterItems} categories={categories} />
+        <Menu items={menu} />
       </section>
     </main>
   )
--- src/Categories.js
+++ src/Categories.js
@@ -5,7 +5,14 @@ const Categories = ({ filterItems, categories }) => {
     <div className="btn-container">
       {categories.map((category, index) => {
         return (
-          
+          <button
+            type='button'
+            className='filter-btn'
+            key={index}
+            onClick={() => filterItems(category)}
+          >
+            {category}
+          </button>
         )
       })}
     </div>
